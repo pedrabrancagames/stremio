65eaa917ff206ef6164c74d3be1ed2aaa01ade4c
Riverlan <pedrabrancagames@gmail.com>
2025-11-30 17:09:37 -0300

    Debug: Add global error handler to serverless.js

--- serverless.js
+++ serverless.js
@@ -1,3 +1,15 @@
-const app = require("./server");
+let app;
+try {
+    app = require("./server");
+} catch (e) {
+    console.error("Failed to import server:", e);
+    app = (req, res) => {
+        res.status(500).json({
+            error: "Server initialization failed",
+            message: e.message,
+            stack: e.stack
+        });
+    };
+}
 
 module.exports = app;
